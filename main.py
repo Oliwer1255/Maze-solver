3323e9cc9d46569296b299b16db70b6e7d30525b
Oliwer1255 <Oliwer1125@outlook.com>
2024-04-22 14:10:00 +0200

    Added line drawing functionality

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from window import Window
+from geometry import Point, Line
 
 def main():
     window = Window(800, 600)
+
+    start = Point(300, 500)
+    end = Point(500, 200)
+    line = Line(start, end)
+    window.draw_line(line, "red")
+
+    start = Point(400, 300)
+    end = Point(200, 200)
+    line = Line(start, end)
+    window.draw_line(line, "black")
+
     window.wait_for_close()
 
 main()
