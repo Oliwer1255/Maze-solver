9b11d9fe457cb807424ebb989d2741934aafe9e2
Oliwer1255 <Oliwer1125@outlook.com>
2024-04-22 13:48:13 +0200

    Added empty window

---
+++ window.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    
+    def __init__(self, width, height):
+        self.__root_widget = Tk()
+        self.__root_widget.title = "Maze solver"
+        self.__canvas = Canvas(self.__root_widget, width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
+        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root_widget.update_idletasks()
+        self.__root_widget.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
